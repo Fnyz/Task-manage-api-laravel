776886b89f8538aaa342fe6d3627894c2fac220f
Jez <jezabellebusico@gmail.com>
2026-07-17 16:03:50 +0800

    Testing

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -19,7 +19,7 @@ return Application::configure(basePath: dirname(__DIR__))
         }
     )
     ->withMiddleware(function (Middleware $middleware): void {
-        $middleware->append(\App\Http\Middleware\ReadOnlyMode::class);
+        // $middleware->append(\App\Http\Middleware\ReadOnlyMode::class);
     })
     ->withExceptions(function (Exceptions $exceptions): void {
         $exceptions->render(function (NotFoundHttpException $e, $request) {
